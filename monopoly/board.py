0aea7f94437fa54b5b5bcbf58055a26f1965e6f2
thomas@leese.io <thomas@leese.io>
2018-09-06 20:42:02 +0100

    Open Yaml configuration files

--- monopoly/board.py
+++ monopoly/board.py
@@ -1,13 +1,32 @@
+from pathlib import Path
+
+import yaml
+
 from . import spaces
 
 
 class Board:
 
-    def __init__(self):
-        self.spaces = [
-            spaces.Go(),
-        ]
+    def __init__(self, filename):
+        self.path = Path(filename)
 
-    @classmethod
-    def from_config(cls, filename):
-        return cls()
+        self.load_rules()
+        self.load_properties()
+        self.load_cards()
+        self.load_spaces()
+
+    def load_config(self, part):
+        with (self.path / f'{part}.yaml').open('r') as file:
+            return yaml.safe_load(file)
+
+    def load_rules(self):
+        config = self.load_config('rules')
+
+    def load_properties(self):
+        config = self.load_config('properties')
+
+    def load_cards(self):
+        config = self.load_config('cards')
+
+    def load_spaces(self):
+        config = self.load_config('spaces')
--- tests/test_board.py
+++ tests/test_board.py
@@ -2,4 +2,4 @@ from monopoly import Board
 
 
 def test_loads_uk_london():
-    assert Board.from_config('uk_london')
+    assert Board('boards/uk_london')
